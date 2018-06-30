2ecb43cd26f24236f5a121be959c545a22f7e5eb
khaledjamal <javaobjects@gmail.com>
2018-06-30 17:26:02 +0400

    improved css style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/mergeMap';
-import {ClientSearchService} from './client-search.service'
+import { ClientSearchService } from './client-search.service'
 import { Client } from './client';
 
 @Component({
@@ -19,26 +19,30 @@ import { Client } from './client';
   styleUrls: ['app.component.css']
 })
 export class AppComponent implements OnInit {
-  items:Array<string>;
-  rows:Array<string>;
-  clients:Array<Client>;
+  items: Array<string>;
+  rows: Array<string>;
+  clients: Array<Client>;
 
   // $ is used to indicate an Observable
   searchTerm$ = new Subject<string>();
 
-  constructor(private clientSearchService:ClientSearchService) {}
+  constructor(private clientSearchService: ClientSearchService) { }
 
   ngOnInit() {
     this.clientSearchService.search(this.searchTerm$).subscribe(results => this.clients = results);
   }
 
   getClientContainerClass(i) {
-    let evenOrOdd = this.isEven(i+1) ? "even" : "odd";
+    let evenOrOdd = this.isEven(i + 1) ? "even" : "odd";
     let result = "client-container client-container-" + evenOrOdd;
-    return result ;
+    return result;
   }
+
+  // getParentContainer() {
+  //   return "parent-container";
+  // }
 
   isEven(n) {
     return n % 2 == 0;
- }
+  }
 }
